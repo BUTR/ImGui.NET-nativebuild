352839bb54fa52d28a04d449169ad6e10e375d31
Vitalii Mikhailov <personal@aragas.org>
2025-03-30 18:57:00 +0300

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -2,10 +2,9 @@
 
 scriptPath="`dirname \"$0\"`"
 RTYPE="${1}"
-RARCH="${2}"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   $scriptPath/build-native.sh ${RTYPE} -osx-architectures 'arm64;x86_64'
 else
-  $scriptPath/build-native.sh ${RTYPE} -arch ${RARCH}
+  $scriptPath/build-native.sh ${RTYPE}
 fi
